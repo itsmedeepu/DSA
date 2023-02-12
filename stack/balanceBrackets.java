ac9ff41b272279a01af16d63b3c15f10a034b886
itsmedeepu <itsmedeepu2021@gmail.com>
2023-02-12 10:45:22 +0530

    updated balanced string

--- stack/balanceBrackets.java
+++ stack/balanceBrackets.java
@@ -1,49 +1,53 @@
-// import java.util.Scanner;
-// import java.util.Stack;
+import java.util.Scanner;
+import java.util.Stack;
 
-// public class balanceBrackets {
+public class balanceBrackets {
 
-//     // [(a+b) + {(c+d)*(e/f)}] ----- true
-//     // [(a+b) + {(c+d)*(e/f)]} ---- false
+    // [(a+b) + {(c+d)*(e/f)}] ----- true
+    // [(a+b) + {(c+d)*(e/f)]} ---- false
 
-//     public static void main(String[] args) {
-//         Scanner obj = new Scanner(System.in);
-//         System.out.println("enter string expression");
-//         String str = obj.nextLine();
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("enter string expression");
+        String str = obj.nextLine();
+        System.out.println(balance(str));
+    }
 
-//         Stack<Character> st = new Stack<>();
-//         for (int i = 0; i < str.length(); i++) {
-//             char ch = str.charAt(i);
-//             if (ch == '(' || ch == '{' || ch == '[') {
-//                 st.push(ch);
-//             } else if (ch == ')') {
-//                 handleClosing
-//             } else if (ch == '}') {
+ //pending
+        public static boolean balance(String str){
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else if (ch == ')' || ch=='}'||ch==']') {
 
-//             } else if (ch == ']') {
+                if(st.isEmpty() || !checkpair(st.pop(),ch)) return false;
+            } 
+        }
+        return st.isEmpty();
+            
+        }
 
-//             }
-//         }
-
-//  pending
-
-
+        public static  boolean checkpair(char c1,char c2){
+            if(c1=='{' && c2=='}') return true;
+            if(c1=='[' && c2==']') return true;
+            if(c1=='(' && c2==')') return true;
+            return false;
+        }
     
+    // public static handleClosing(stack<Character> st , Char corresOCh){
+    //     if (st.size() == 0) {
+    //         System.out.println("false");
+    //         return false;
 
+    //     } else if (st.peek() != corresOCh ) {
+    //         System.out.println("false");
+    //         return false;
+    //     } else {
+    //         st.pop();
+    //         return true;
+    //     }
+    // }
 
-//     public static handleClosing(stack<Character> st , Char corresOCh){
-//         if (st.size() == 0) {
-//             System.out.println("false");
-//             return false;
-
-//         } else if (st.peek() != corresOCh ) {
-//             System.out.println("false");
-//             return false;
-//         } else {
-//             st.pop();
-//             return true;
-//         }
-//     }
-
-// }
-// }
+}
